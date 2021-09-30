3fdb1e00316ee37e1859271a7677452bc51be425
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-09-30 21:26:28 +0530

    merged previous 5 commits into one

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a className="navbar-brand" href="/">
+            Sportify
+          </a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  <span className="fa fa-home"></span> Home
+                  <span className="sr-only"></span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about-us">
+                  <span className="fa fa-info"></span> About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <span className="fa fa-address-book"></span> Contact Us
+                </a>
+              </li>
+            </ul>
+            <span className="navbar-text ms-auto p-3">
+              <a href="/register">
+                <span className="fa fa-user-plus"></span> Register
+              </a>
+            </span>
+            <span className="navbar-text p-3">
+              <a href="/">
+                <span className="fa fa-sign-in-alt" aria-hidden="true"></span>{" "}
+                Login
+              </a>
+            </span>
+            <span className="navbar-text p-3">
+              <a href="/">
+                <i class="fa fa-lg fa-shopping-cart"></i>
+              </a>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ client/src/screens/Homescreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Homescreen() {
+  return (
+    <div>
+      <h1>Home Screen Page</h1>
+    </div>
+  );
+}
