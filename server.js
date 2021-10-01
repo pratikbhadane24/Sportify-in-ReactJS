217d599512303765f677bed0109888d76bee0cd2
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-01 11:16:17 +0530

    formatted

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const app = express();
 
-var dbconnection = require('./db')
+var dbconnection = require("./db");
 
 app.get("/", (req, res) => {
   res.send("This is from Backend");
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port http://localhost:${port} 🔥`));
+app.listen(port, () =>
+  console.log(`Server running on port http://localhost:${port} 🔥`)
+);
