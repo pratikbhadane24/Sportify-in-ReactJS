b7f54f42b7ffd1408e0486f717a1d2789801b3d3
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-01 12:58:53 +0530

    product routing complete

---
+++ routes/productRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/productModel");
+
+router.get("/getallproducts", (req, res) => {
+  Product.find({}, (err, docs) => {
+    if (!err) {
+      return res.json({ data: docs });
+    } else {
+      return res.status(400).json({ message: "Something went wrong!" });
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 var dbconnection = require("./db");
+var productRoute = require("./routes/productRoute");
+
+app.use("/api/products/", productRoute);
 
 app.get("/", (req, res) => {
   res.send("This is from Backend");
