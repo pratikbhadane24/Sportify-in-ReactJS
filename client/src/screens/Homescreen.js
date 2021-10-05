37e5aae5c27bf119b32855e9077493a6242e3552
Chris <chrishonsalves@gmail.com>
2021-10-05 17:13:28 +0530

    Added Carousel

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -24,6 +24,37 @@ export default function Homescreen() {
 
   return (
     <div>
+      <section className="m-2 pt-5">
+        <div className="container">
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <a href="/product/615634397db4e95a0b48b7db">
+                    <img src="https://i.ibb.co/GVZxJQc/Slide1.png" className="d-block w-100" alt="..."/></a>
+                  </div>
+                  <div className="carousel-item">
+                    <img src="https://i.ibb.co/GVZxJQc/Slide1.png" className="d-block w-100" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                    <img src="https://i.ibb.co/GVZxJQc/Slide1.png" className="d-block w-100" alt="..."/>
+                  </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+              </div>
+        </div>
+    </section>
       <Filter />
       <div className="row justify-content-center">
         {loading ? (
