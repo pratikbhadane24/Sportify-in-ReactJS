167dc4c1afe95ef58115808d06fbe0593e4bf05f
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-11 20:54:26 +0530

    fixed cart bug

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -13,6 +13,10 @@ export const addToCart = (product, quantity) => (dispatch, getState) => {
   );
 };
 
-export const deleteFromCart = (item) => (dispatch) => {
+export const deleteFromCart = (item) => (dispatch, getState) => {
   dispatch({ type: "DELETE_FROM_CART", payload: item });
+  localStorage.setItem(
+    "cartItems",
+    JSON.stringify(getState().cartReducer.cartItems)
+  );
 };
