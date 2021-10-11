785cf6e1c4e3fc98664adca96c8e9be0da0989e8
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-11 20:25:02 +0530

    styled the filter search

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,19 +1,20 @@
-  import React, { useState } from "react";
-  import { useDispatch } from "react-redux";
-  import { filterProducts } from "../actions/productActions";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { filterProducts } from "../actions/productActions";
 
-  export default function Filter() {
-    const [searchkey, setsearchkey] = useState("");
-    const [sort, setsort] = useState("popular");
-    const [category, setcategory] = useState("all");
+export default function Filter() {
+  const [searchkey, setsearchkey] = useState("");
+  const [sort, setsort] = useState("popular");
+  const [category, setcategory] = useState("all");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return (
-      <p>
-        <div className="container text-end p-2">
+  return (
+    <p>
+      <div className="container text-end p-2 accordion accordion-flush">
+        <h2 class="accordion-header">
           <button
-            className="btn btn-dark"
+            className="accordion-button collapsed my_accordion"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#collapseExample"
@@ -21,61 +22,62 @@
             aria-controls="collapseExample"
           >
             Search And Filtering
-          </button>
-        </div>
-        <div className="container collapse" id="collapseExample">
-          <div className="row justify-content-center">
-            <div className="col-5 m-2">
-              <input
-                value={searchkey}
-                onChange={(e) => {
-                  setsearchkey(e.target.value);
-                }}
-                type="text"
-                placeholder="Search Products"
-                className="form-control"
-              />
-            </div>
-            <div className="col-2 m-2">
-              <select
-                className="drop-down drop form-control"
-                value={sort}
-                onChange={(e) => {
-                  setsort(e.target.value);
-                }}
-              >
-                <option value="popular">Popular</option>
-                <option value="htl">Price High to Low</option>
-                <option value="lth">Price Low to High</option>
-              </select>
-            </div>
-            <div className="col-2 m-2">
-              <select
-                className="drop-down drop form-control"
-                value={category}
-                onChange={(e) => {
-                  setcategory(e.target.value);
-                }}
-              >
-                <option value="all">All</option>
-                <option value="football">Football</option>
-                <option value="cricket">Cricket</option>
-                <option value="basketball">Basketball</option>
-                <option value="tennis">Tennis</option>
-              </select>
-            </div>
-
-            <button
-              className="col-2 m-2 btn btn-dark"
-              onClick={() => {
-                dispatch(filterProducts(searchkey, sort, category));
+          </button>{" "}
+        </h2>
+      </div>
+      <div className="container collapse" id="collapseExample">
+        <div className="row justify-content-center">
+          <div className="col-5 m-2">
+            <input
+              value={searchkey}
+              onChange={(e) => {
+                setsearchkey(e.target.value);
+              }}
+              type="text"
+              placeholder="Search Products"
+              className="form-control"
+            />
+          </div>
+          <div className="col-2 m-2">
+            <select
+              className="drop-down drop form-control"
+              value={sort}
+              onChange={(e) => {
+                setsort(e.target.value);
               }}
             >
-              Filter Search
-            </button>
+              <option value="popular">Popular</option>
+              <option value="htl">Price High to Low</option>
+              <option value="lth">Price Low to High</option>
+            </select>
           </div>
-          <hr />
+          <div className="col-2 m-2">
+            <select
+              className="drop-down drop form-control"
+              value={category}
+              onChange={(e) => {
+                setcategory(e.target.value);
+              }}
+            >
+              <option value="all">All</option>
+              <option value="football">Football</option>
+              <option value="cricket">Cricket</option>
+              <option value="basketball">Basketball</option>
+              <option value="tennis">Tennis</option>
+            </select>
+          </div>
+
+          <button
+            className="col-2 m-2 btn btn-dark"
+            onClick={() => {
+              dispatch(filterProducts(searchkey, sort, category));
+            }}
+          >
+            Filter Search
+          </button>
         </div>
-      </p>
-    );
-  }
+        <hr />
+      </div>
+    </p>
+  );
+}
