a4f5b87ac3dd3c9951c9239857d6767ce3602381
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-14 01:51:37 +0530

    defined the admin route in backend

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -65,4 +65,14 @@ router.post("/update", (req, res) => {
   );
 });
 
+router.get("/getallusers", (req, res) => {
+  User.find({}, (err, docs) => {
+    if (err) {
+      return res.status(400).json({ message: "Something went wrong!" });
+    } else {
+      res.send(docs);
+    }
+  });
+});
+
 module.exports = router;
