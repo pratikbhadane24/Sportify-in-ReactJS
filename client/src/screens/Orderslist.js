d4e5909a5fa162b37386924f54e1b827e4be973b
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-16 00:06:40 +0530

    displaying orderlist on screens

--- client/src/screens/Orderslist.js
+++ client/src/screens/Orderslist.js
@@ -6,16 +6,51 @@ import Error from "../components/Error";
 import { getAllOrders } from "../actions/orderAction";
 
 export default function Orderslist() {
+  const getordersstate = useSelector((state) => state.getAllOrdersReducer);
+  const { loading, error, orders } = getordersstate;
 
-const dispatch = useDispatch()
-    useEffect(() => {
-dispatch(getAllOrder())
-
-    }, [])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllOrders());
+  }, []);
 
   return (
     <div>
-      <h1>This is orders List</h1>
+      <h4 className="text-start ">Order List</h4>
+      {error && <Error error="Something went wrong!" />}
+      <table className="table table-striped table-bordered table-hover mb-5">
+        <thead className="bg-dark text-light">
+          <tr>
+            <th>Order ID</th>
+            <th>Email</th>
+            <th>User ID</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Transaction ID</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <div className="text-center">{loading && <Loader />}</div>
+          {orders &&
+            orders.map((order) => {
+              return (
+                <tr
+                  onClick={() => {
+                    window.location.href = `/orderinfo/${order._id}`;
+                  }}
+                >
+                  <td>{order._id}</td>
+                  <td>{order.email}</td>
+                  <td>{order.userid}</td>
+                  <td>{order.orderAmount}</td>
+                  <td>{order.createdAt}</td>
+                  <td>{order.transactionId}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
