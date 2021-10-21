57332600ad412aa19d6c382305f7a8f57329c50c
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-10-21 12:42:38 +0530

    changed table response

--- client/src/screens/Ordersscreen.js
+++ client/src/screens/Ordersscreen.js
@@ -21,45 +21,51 @@ export default function Ordersscreen() {
   return (
     <div>
       <div className="row mt-5 justify-content-center">
-        <div className="col-md-8 shadow-lg p-5 mb-5 bg-body rounded table-responsive">
+        <div className="col-md-8 shadow-lg p-5 mb-5 bg-body rounded">
           <h3 className="m-5">My Orders</h3>
-          <table className="table table-striped table-bordered table-hover mb-5">
-            <thead className="bg-dark text-light">
-              <tr>
-                <th>Order ID</th>
-                <th>Amount</th>
-                <th>Date </th>
-                <th>Transaction ID</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders &&
-                orders.map((order) => {
-                  return (
-                    <tr
-                      onClick={() => {
-                        window.location = `/orderinfo/${order._id}`;
-                      }}
-                    >
-                      <td>{order._id}</td>
-                      <td>₹{order.orderAmount}</td>
-                      <td>{order.createdAt.substring(0, 10)}</td>
-                      <td>{order.transactionId}</td>
-                      <td>
-                        {order.isDelivered ? (
-                          <li className="list-group-item bg-success">Delivered</li>
-                        ) : (
-                          <li className="list-group-item bg-info">Order Placed</li>
-                        )}
-                      </td>
-                    </tr>
-                  );
-                })}
-              {error && <Error error="Something went Wrong." />}
-            </tbody>
-          </table>
+          <div className="table-responsive">
+            <table className="table table-striped table-bordered table-hover mb-5">
+              <thead className="bg-dark text-light">
+                <tr>
+                  <th>Order ID</th>
+                  <th>Amount</th>
+                  <th>Date </th>
+                  <th>Transaction ID</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders &&
+                  orders.map((order) => {
+                    return (
+                      <tr
+                        onClick={() => {
+                          window.location = `/orderinfo/${order._id}`;
+                        }}
+                      >
+                        <td>{order._id}</td>
+                        <td>₹{order.orderAmount}</td>
+                        <td>{order.createdAt.substring(0, 10)}</td>
+                        <td>{order.transactionId}</td>
+                        <td>
+                          {order.isDelivered ? (
+                            <li className="list-group-item bg-success">
+                              Delivered
+                            </li>
+                          ) : (
+                            <li className="list-group-item bg-info">
+                              Order Placed
+                            </li>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                {error && <Error error="Something went Wrong." />}
+              </tbody>
+            </table>
           {loading && <Loader />}
+          </div>
         </div>
       </div>
       <br /> <br />
